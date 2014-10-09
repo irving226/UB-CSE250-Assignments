0bf7cdc0366d17b9298daa9a2d57599bb498bce0
James Droste <jamesdro@buffalo.edu>
2014-10-09 00:44:51 -0400

    A5: Added Problem 2/3 tests.
    - Updated Problem 3 (sum_to_target)'s prototype. Still unsure on approaching it.

--- A5/a5.h
+++ A5/a5.h
@@ -16,8 +16,8 @@ size_t iterative_num_negatives(stack<int> int_stack);
 size_t recursive_num_negatives(stack<int> int_stack);
 
 // Problem 2
-int iterative_sub_vector(vector<int>& a, vector<int> b);
-int recursive_sub_vector(vector<int>& a, vector<int> b, size_t k);
+bool iterative_sub_vector(vector<int>& a, vector<int> b);
+bool recursive_sub_vector(vector<int>& a, vector<int> b, size_t k);
 
 // Problem 3
 bool sum_to_target(vector<int>& int_vec, int target);
--- A5/runner.cpp
+++ A5/runner.cpp
@@ -21,6 +21,7 @@ stack<int> p1_makeStack(int neg);
 void test_p2_a();
 void test_p2_b();
 void test_p3();
+vector<int> p3_makeVector(int amount);
 
 // Methods
 int main(int argc, char **argv)  {
@@ -60,7 +61,7 @@ void test_p1_a() {
 			fail++;
 		}
 		
-		cout << "[test_p1_a : test" << i << " | " << pfail << " ] expected: " << neg << "  | actual: " << actual << endl;
+		cout << "[test_p1_a : test" << i << " | " << pfail << " ] expected: " << neg << " | actual: " << actual << endl;
 	}
 	
 	if ( fail > 0 )
@@ -82,33 +83,146 @@ void test_p1_b() {
 			fail++;
 		}
 		
-		cout << "[test_p1_b : test" << i << " | " << pfail << " ] expected: " << neg << "  | actual: " << actual << endl;
+		cout << "[test_p1_b : test" << i << " | " << pfail << " ] expected: " << neg << " | actual: " << actual << endl;
 	}
 	
 	if ( fail > 0 )
 		cout << endl << "FAILED: " << fail << endl;
 }
 
-void test_p2_a() { }
+void test_p2_a() {
+	string pfail1 = "FAIL"; string pfail2 = "FAIL";
+	int fail = 2;
+	
+	// Generate our vectors
+	vector<int> masterVec;
+	vector<int> test1;
+	vector<int> test2;
+	
+	masterVec.push_back(1);masterVec.push_back(2);masterVec.push_back(3);masterVec.push_back(1);masterVec.push_back(4);masterVec.push_back(5);
+	test1.push_back(2);test1.push_back(3);test1.push_back(1);
+	test2.push_back(3);test2.push_back(1);test2.push_back(5);
+	
+	// Expected
+	bool expected1 = true;
+	bool expected2 = false;
+	
+	// Actual
+	bool actual1 = iterative_sub_vector(masterVec, test1);
+	bool actual2 = iterative_sub_vector(masterVec, test2);
+	
+	// Update passfail
+	if ( expected1 == actual1 ) { pfail1 = "PASS"; fail--; }
+	if ( expected2 == actual2 ) { pfail2 = "PASS"; fail--; }
+	
+	cout << "[test_p2_a : test1 | " << pfail1 << " ] expected: " << expected1 << " | actual: " << actual1 << endl;
+	cout << "[test_p2_a : test2 | " << pfail2 << " ] expected: " << expected2 << " | actual: " << actual2 << endl;
+	
+	if ( fail > 0 )
+		cout << endl << "FAILED: " << fail << endl;
+}
 
-void test_p2_b() { }
+void test_p2_b() {
+	string pfail1 = "FAIL"; string pfail2 = "FAIL";
+	int fail = 2;
+	
+	// Generate our vectors
+	vector<int> masterVec;
+	vector<int> test1;
+	vector<int> test2;
+	
+	masterVec.push_back(1);masterVec.push_back(2);masterVec.push_back(3);masterVec.push_back(1);masterVec.push_back(4);masterVec.push_back(5);
+	test1.push_back(2);test1.push_back(3);test1.push_back(1);
+	test2.push_back(3);test2.push_back(1);test2.push_back(5);
+	
+	// Expected
+	bool expected1 = true;
+	bool expected2 = false;
+	
+	// Actual
+	bool actual1 = recursive_sub_vector(masterVec, test1, 0);
+	bool actual2 = recursive_sub_vector(masterVec, test2, 0);
+	
+	// Update passfail
+	if ( expected1 == actual1 ) { pfail1 = "PASS"; fail--; }
+	if ( expected2 == actual2 ) { pfail2 = "PASS"; fail--; }
+	
+	cout << "[test_p2_b : test1 | " << pfail1 << " ] expected: " << expected1 << " | actual: " << actual1 << endl;
+	cout << "[test_p2_b : test2 | " << pfail2 << " ] expected: " << expected2 << " | actual: " << actual2 << endl;
+	
+	if ( fail > 0 )
+		cout << endl << "FAILED: " << fail << endl;
+}
 
-void test_p3() { }
+void test_p3() {
+	int fail = 0;
+	
+	for ( int i=1; i < 10; i++ ) {
+		int amount = rand() % 1000;
+		vector<int> vec = p3_makeVector(amount);
+		string pfail = "FAIL";
+		
+		int target;
+		bool expected;
+		
+		if ( (rand() % 100) > 50 || amount == 0 ) {
+			target = amount;
+			expected = true;
+		} else {
+			target = amount / 2;
+			expected = false;
+		}
+		
+		int actual = sum_to_target(vec, target);
+		
+		if ( expected == actual ) {
+			pfail = "PASS";
+		} else {
+			fail++;
+		}
+		
+		cout << "[test_p3 : test" << i << " | " << pfail << " ] expected: " << expected << " | actual: " << actual << endl;
+	}
+	
+	if ( fail > 0 )
+		cout << endl << "FAILED: " << fail << endl << endl;
+}
 
 // Helper Methods
 stack<int> p1_makeStack(int amt) {
 	stack<int> s;
-	int neg = 0;
 
 	for ( int i=0; i < (amt*2); i++ ) {
 		int num = rand() % 100 + 1; // Pesky 0's
 		if ( i % 2 ) {
 			s.push((num * -1));
-			neg++;
 		} else {
 			s.push(num);
 		}
 	}
 	
 	return s;
+}
+
+vector<int> p3_makeVector(int amount) {
+	vector<int> v;
+	int needed = amount;
+	
+	for ( int i=0; i < amount; i++ ) {
+		int num1 = rand() % 100 + 1; // Pesky 0's
+		int num2 = rand() % 10 + 1;
+		
+		if ( i % 2 ) {
+			if ( needed - num2 > 0 ) {
+				v.push_back(needed-num2);
+				needed -= num2;
+			} else {
+				v.push_back(num1);
+			}
+		} else {
+			v.push_back(num1);
+		}
+	}
+	
+	return v;
 }
